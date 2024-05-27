7bed513702bd5123dbf1107fceada31987245485
CemshiCoban <denizcemal@hotmail.com>
2024-05-27 17:32:44 +0300

    changed usersetting model

--- src/models/UserSettings.ts
+++ src/models/UserSettings.ts
@@ -4,7 +4,6 @@ export interface IUserSettings extends Document {
   email: string;
   maxCpu: number;
   maxDisk: number;
-  alertCount: number;
   createdAt: Date;
 }
 
@@ -12,7 +11,6 @@ const UserSettingsSchema: Schema = new Schema({
   email: { type: String, required: true },
   maxCpu: { type: Number, required: true },
   maxDisk: { type: Number, required: true },
-  alertCount: { type: Number, default: 0 },
   createdAt: { type: Date, default: Date.now }
 });
 
